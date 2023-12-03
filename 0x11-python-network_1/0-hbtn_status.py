60f686d18ab30abc1ec031fd1a13adafc6fd0b2b
Emmanuel Ndua <emmanuel.nduan@gmail.com>
2023-12-03 09:37:24 -0800

    new2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""Bash script gets the status of https://alx-intranet.hbtn.io using urllib"""
-
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
 
-def fetch_status():
-    """GETS the status of the URL & SHOWS the response body"""
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
-
-
-if __name__ == '__main__':
-    fetch_status()
